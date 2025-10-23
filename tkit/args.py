c1d7b58b0005355b8cdc4399478315592bb8d866
v0idm4sk <voidmaskyt@gmail.com>
2025-10-23 18:50:46 -0400

    TKit!

--- tkit/args.py
+++ tkit/args.py
@@ -15,5 +15,3 @@ class Command:
             else:
                 if np.any(np.isin(np.array(sys.argv), np.array(opt["arguments"].split()))):
                     opt["func"](*opt["arguments"].split())
-                else:
-                    print("\033[31m× \033[0mTKit: \033[31mArgs Error: \033[0mThe arguments are insufficient!\nYou have 2 options:\n  - 1. Declare 'require-args' as False.\n  - 2. Add an exception message (with the argument 'exception').")
